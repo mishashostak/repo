ad389e213ea1e633fce10506bf587bb818d96506
mishashostak <114440497+mishashostak@users.noreply.github.com>
2022-10-19 02:14:51 -0400

    Initial commit

---
+++ Rectangle.java
@@ -0,0 +1,19 @@
+public class Rectangle {
+    public int x1,y1,x2,y2;
+    public boolean filled;
+    public int getX1() {
+        return x1;
+    }
+    public int getY1() {
+        return y1;
+    }
+    public int getX2() {
+        return x2;
+    }
+    public int getY2() {
+        return y2;
+    }
+    public boolean getFilledlol() {
+        return filled;
+    }
+}
