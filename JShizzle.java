2077055ec15cae059139f50399697e76efce68e6
mishashostak <114440497+mishashostak@users.noreply.github.com>
2022-10-31 12:37:09 -0400

    virtual agent upd

---
+++ JShizzle.java
@@ -0,0 +1,10 @@
+public class JShizzle extends VirtualAgent {
+    public JShizzle(String text) {
+//body
+    }
+
+    @Override
+    public String respondToUserQuery(String userQuery) {
+//body
+    }
+}
