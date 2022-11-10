ccbea5b653e785a45b25b9f80bfd55c060ee8356
mishashostak <114440497+mishashostak@users.noreply.github.com>
2022-11-09 23:28:47 -0500

    3-2 fini

--- ThreeTwoN2.java
+++ ThreeTwoN2.java
@@ -11,6 +11,7 @@ import java.util.*;
  */
 public class ThreeTwoN2 {
     private static String[] nArr;
+    
     /**
      * Main method to test the program
      * 
@@ -22,10 +23,7 @@ public class ThreeTwoN2 {
     }
 
     /**
-     * This method creates an array from an input amount of inputted integers 
-     * then converts the array into a string to be returned
-     * 
-     * @return String - The Array presented as a String
+     * Mutator to define String Array
      */
     public static void setNumArr() {
         System.out.println("\nInput the desired amount of different integers you'd like to input:");
@@ -35,26 +33,34 @@ public class ThreeTwoN2 {
                 System.out.println("\nHow is that remotely an integer?\nTry again and don't be an idiot this time:");
                 hold = input.nextLine();
             }
-            while (hold.substring(0) == "0" && hold.length()>1) {
-                System.out.println("\nStop mucking about\nTry again and don't be a complete moron this time:");
-                hold = input.nextLine();
-            }
             nArr = new String[Integer.parseInt(hold)];
-            System.out.printf("%nInput %d different integers:%n", nArr.length);
-            do {
-                String plac = input.nextLine();
-                for (int i = 0; i<=nArr.length-1; i++) {
-                    while(Arrays.asList(nArr).contains(plac)) {
-                        System.out.println("\nI said DIFFERENT!!! ...god you're frustrating\nTry again and don't be an idiot this time:");
-                    }
-                    nArr[i] = plac;
-                    System.out.println("\nNext please:");
+            System.out.printf("%nInput %d different integers:%n", Integer.parseInt(hold));
+            String plac = input.nextLine();
+            for (int i = 0; i<=nArr.length-1; i++) {
+                while(Arrays.asList(nArr).contains(plac)) {
+                    System.out.println("\nI said DIFFERENT!!! ...god you're frustrating\nTry again and don't be an idiot this time:");
                     plac = input.nextLine();
                 }
-            } while(true);
-        }
+                while(!plac.matches("[0-9]+")) {
+                    System.out.println("\nExplain how that's an integer? Absolute imbecile.\nTry again and make sure to use your brain cells:");
+                    plac = input.nextLine();
+                }
+                nArr[i] = plac;
+                if (i == nArr.length-1) {
+                    break;
+                } else {
+                    System.out.println("\nNext please:");
+                }
+                plac = input.nextLine();
+            }
+        } 
     }
 
+    /**
+     * This method returns the String Array in a formatted String
+     * 
+     * @return String - The String representation of the String Array
+     */
     public static String arrToString() {
         StringBuilder strB = new StringBuilder("\n[");
         for (String st : nArr){
