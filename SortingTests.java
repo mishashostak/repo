e3c1ec28c02c6e9aa70b8783a0d08af5239d5037
mishashostak <tasmainianstampy@gmail.com>
2022-11-22 10:22:14 -0500

    quick sort analysis tbd

--- SortingTests.java
+++ SortingTests.java
@@ -3,7 +3,7 @@ public class SortingTests {
         long start, finish;
         
         // create object to perform Selection Sort
-        SelectionSort selectArr = new SelectionSort( 100000);
+        SelectionSort selectArr = new SelectionSort( 100000, SelectionSort.DESCENDING );
         
         System.out.println( "Selection Sort (100000 randomly ordered integers): " );
         start = System.currentTimeMillis();
@@ -16,7 +16,7 @@ public class SortingTests {
         finish=0;
 
         // create object to perform Bubble Sort
-        BubbleSort bubbleArr = new BubbleSort( 10, BubbleSort.RANDOM );
+        BubbleSort bubbleArr = new BubbleSort( 100000, BubbleSort.DESCENDING );
 
         System.out.println( "Bubble Sort (100000 randomly ordered integers): " );
         start = System.currentTimeMillis();
@@ -28,8 +28,15 @@ public class SortingTests {
          * Bubble sorting is far quicker
          * than Selection sorting
          * 
-         * Bubble       Selection
-         *  0 ms         2163 ms
+         * (**testing done on horrid school computer**)
+         * (approx. 10 trials done for each type of data set)
+         *   Bubble                 Selection
+         *               [RANDOM]
+         *  16083-16340 ms          4268-4666 ms  
+         *              [ASCENDING]
+         *   1-3 ms                 1554-1661 ms    
+         *              [DESCENDING]
+         *  6871-7233 ms            2884-4434 ms    
          */
     }
 }
